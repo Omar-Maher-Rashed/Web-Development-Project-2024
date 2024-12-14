61e71b073c422d46f17c77cfdfd1e64b75261073
Omar-Maher-Rashed <omar.m.rashed@outlook.com>
2024-12-14 16:57:47 +0200

    Add files via upload

---
+++ project 1/script.js
@@ -0,0 +1,16 @@
+let mybutton = document.getElementById("scrollTopBtn");
+
+window.onscroll = function() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+};
+
+mybutton.onclick = function() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+};
